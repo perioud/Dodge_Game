fe041a1231d8fbe7b543cb505cc6c820378234d5
Hoya <kkinggola@gmail.com>
2024-04-09 17:36:19 +0900

    0409_Upload

--- Dodge/Assets/PlyaerController.cs
+++ Dodge/Assets/PlyaerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlyaerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRigidbody;
+    public float speed = 8f; //이동 속력
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        /**
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+        **/
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xspeed = xInput * speed;
+        float zspeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xspeed, 0f, zspeed);
+
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
